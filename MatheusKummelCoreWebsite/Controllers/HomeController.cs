9f3fe6f3c5b2e097e13e6e3a211381c8325534d8
Matheus K <me@matheuskummel.com>
2018-03-08 11:39:54 -0300

    V1.0 - Website
    First commit to github

---
+++ MatheusKummelCoreWebsite/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MatheusKummelCoreWebsite.Models;
+
+namespace MatheusKummelCoreWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[Localization("en")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[Localization("en")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "About Matheus";
+
+            return View();
+        }
+    }
+}
